6d82a398aee8e145b8430372fc96137475b99064
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2020-12-31 02:04:05 -0700

    Create abilities.ts

---
+++ data/mods/poultergeist/abilities.ts
@@ -0,0 +1,8 @@
+export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
+	chickenout: {
+		shortDesc: "When its HP reaches 0, this Pokémon retreats to the party, and then...",
+		name: "Chicken Out",
+		rating: 5,
+		num: -1001,
+	},
+};
