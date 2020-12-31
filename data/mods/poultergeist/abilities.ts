9c187326d2e9ecba6687d7d66d56ec853dab5ff6
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2020-12-31 04:29:39 -0700

    Update abilities.ts

--- data/mods/poultergeist/abilities.ts
+++ data/mods/poultergeist/abilities.ts
@@ -5,6 +5,7 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 		onFaint(pokemon) {
 			if (pokemon.headless === true) {
 				if (pokemon.formeChange('Poultergeist-Headless', this.effect, true)) {
+					this.add('-ability', pokemon, 'Chicken Out');
 					this.add('-message', `${pokemon.name} ran off somewhere...`);
 					pokemon.maxhp = 1;
 					pokemon.hp = 1;
