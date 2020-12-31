55fc54dd3f38df1b4b2cd671d68490be669a27f5
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2020-12-31 02:56:41 -0700

    Update abilities.ts

--- data/mods/poultergeist/abilities.ts
+++ data/mods/poultergeist/abilities.ts
@@ -2,6 +2,11 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 	chickenout: {
 		shortDesc: "When its HP reaches 0, this Pokémon retreats to the party, and then...",
 		name: "Chicken Out",
+		onFaint(pokemon) {
+			if (this.canSwitch(pokemon.side)) {
+				this.add('-ability', pokemon, 'Chicken Out');
+			}
+		},
 		rating: 5,
 		num: -1001,
 	},
