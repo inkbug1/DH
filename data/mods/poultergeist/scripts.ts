0972ec5afbcdb0dc7d26e2ad959dfa846401cbe5
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2020-12-31 02:38:48 -0700

    Update scripts.ts
    Fixing this formatting but actually I'm gonna double-check something

--- data/mods/poultergeist/scripts.ts
+++ data/mods/poultergeist/scripts.ts
@@ -3,18 +3,18 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 		for (const side of this.sides) {
 			for (const pokemon of side.active) {
 				if (pokemon.fainted) {
-          if (pokemon.ability === 'chickenout') {
-	      		if (pokemon.species.baseSpecies !== 'Poultergeist' || pokemon.transformed) {
-              pokemon.status = 'fnt' as ID;
-              pokemon.switchFlag = true;
-            }
-            pokemon.headless = true;
-	  				pokemon.fainted = false;
-            pokemon.switchFlag = true;
-          } else {
-  					pokemon.status = 'fnt' as ID;
-	  				pokemon.switchFlag = true;
-          }
+					if (pokemon.ability === 'chickenout') {
+						if (pokemon.species.baseSpecies !== 'Poultergeist' || pokemon.transformed) {
+							pokemon.status = 'fnt' as ID;
+							pokemon.switchFlag = true;
+						}
+						pokemon.headless = true;
+						pokemon.fainted = false;
+						pokemon.switchFlag = true;
+					} else {
+						pokemon.status = 'fnt' as ID;
+						pokemon.switchFlag = true;
+					}
 				}
 			}
 		}
