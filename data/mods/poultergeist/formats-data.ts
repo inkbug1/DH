21d3bb68c2452de97a19de1d3e9e1bada4b188a2
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2020-12-31 02:50:47 -0700

    Create formats-data.ts

---
+++ data/mods/poultergeist/formats-data.ts
@@ -0,0 +1,10 @@
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
+	poultergeist: {
+		tier: "UU",
+		doublesTier: "UU",
+	},
+	poultergeist-headless: {
+		tier: "UU",
+		doublesTier: "UU",
+	},
+};
