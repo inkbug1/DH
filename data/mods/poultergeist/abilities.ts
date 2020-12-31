5b410709af2b77d1eea038982ad28184fd0a105f
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2020-12-31 14:29:10 -0700

    Update abilities.ts

--- data/mods/poultergeist/abilities.ts
+++ data/mods/poultergeist/abilities.ts
@@ -2,29 +2,13 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 	chickenout: {
 		shortDesc: "When its HP reaches 0, this Pokémon retreats to the party, and then...",
 		name: "Chicken Out",
-		onDamagePriority: -100,
-		onDamage(damage, target, source, effect) {
-			if (damage >= target.hp && target.ability === 'chickenout' && !target.transformed && !target.headless && this.canSwitch(target.side)) {
-				target.headless = true;
-				this.runEvent('Faint', target, source, effect);
-				target.cureStatus('[silent]');
-				target.volatiles = {};
-				target.illusion = null;
-				target.isActive = false;
-				target.isStarted = false;
-				target.switchFlag = true;
-				target.side.faintedThisTurn = target;
-				return target.hp - 1;
-			}
-		},
-		onSwitchOut(pokemon) {
-			if (pokemon.headless) {
+		onFaint(pokemon) {
+			if (pokemon.headless === true) {
 				if (pokemon.formeChange('Poultergeist-Headless', this.effect, true)) {
 					this.add('-ability', pokemon, 'Chicken Out');
 					this.add('-message', `${pokemon.name} ran off somewhere...`);
 					pokemon.maxhp = 1;
 					pokemon.hp = 1;
-					delete pokemon.headless;
 				}
 			}
 		},
