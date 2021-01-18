899e28b9c79dddbd6287370bd182584da0f8e75e
inkbug1 <72170979+inkbug1@users.noreply.github.com>
2021-01-17 18:07:10 -0800

    abilities wip
    still haveto do moves dot ts and also items dot ts

--- data/mods/conniecorddraft/abilities.ts
+++ data/mods/conniecorddraft/abilities.ts
@@ -25,4 +25,140 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 		rating: 5,
 		num: -1001,
 	},
+	dialogue: {
+		name: "Dialogue",
+		num: -1002,
+		
+	},
+	frozenpower: {
+		name: "Frozen Power",
+		num: -1003,
+		
+	},
+	permafrost: {
+		name: "Permafrost", 
+		num: -1004,
+		onStart(pokemon) {
+			let activated = false;
+			for (const target of pokemon.side.foe.active) {
+				if (!target || !this.isAdjacent(target, pokemon)) continue;
+				if (!activated) {
+					this.add('-ability', pokemon, 'Permafrost', 'boost');
+					activated = true;
+				}
+				if (target.volatiles['substitute']) {
+					this.add('-immune', target);
+				} else {
+					this.boost({spe: -1}, target, pokemon, null, true);
+				}
+			}
+		},
+	},
+	grampandemonium: {
+		name: "Grampandemonium", 
+		num: -1005,
+		
+	},
+	stonilate: {
+		name: "Stonilate", 
+		num: -1006,
+		shortDesc: "Normal attacks become Rock; 1.3x power.",
+		onModifyTypePriority: -1,
+		onModifyType(move, pokemon) {
+			const noModifyType = [
+				'judgment', 'multiattack', 'naturalgift', 'revelationdance', 'technoblast', 'terrainpulse', 'weatherball',
+			];
+			if (move.type === 'Normal' && !noModifyType.includes(move.id) && !(move.isZ && move.category !== 'Status')) {
+				move.type = 'Rock';
+				move.refrigerateBoosted = true;
+			}
+		},
+		onBasePowerPriority: 23,
+		onBasePower(basePower, pokemon, target, move) {
+			if (move.refrigerateBoosted) return this.chainModify([0x1333, 0x1000]);
+		},
+	},
+	shpeedforce: {
+		name: "Shpeed Force",
+		num: -1007,
+		shortDesc: "Electric power 1.5x. At the end of each turn, +1 Spe.",
+		onModifyAtkPriority: 5,
+		onModifyAtk(atk, attacker, defender, move) {
+			if (move.type === 'Electric') {
+				this.debug('Transistor boost');
+				return this.chainModify(1.5);
+			}
+		},
+		onModifySpAPriority: 5,
+		onModifySpA(atk, attacker, defender, move) {
+			if (move.type === 'Electric') {
+				this.debug('Transistor boost');
+				return this.chainModify(1.5);
+			}
+		},
+		onResidualOrder: 26,
+		onResidualSubOrder: 1,
+		onResidual(pokemon) {
+			if (pokemon.activeTurns) {
+				this.boost({spe: 1});
+			}
+		},
+	},
+	viciousjaw: {
+		name: "Vicious Jaw", 
+		num: -1008,
+		
+	},
+	playtime: {
+		name: "Playtime",
+		num: -1009,
+		onPrepareHit(source, target, move) {
+			if (move.category === 'Status' || move.selfdestruct || move.multihit) return;
+			if (['endeavor', 'fling', 'iceball', 'rollout'].includes(move.id)) return;
+			if (!move.flags['charge'] && !move.spreadHit && !move.isZ && !move.isMax) {
+				move.multihit = 2;
+				move.multihitType = 'parentalbond';
+			}
+		},
+		onBasePowerPriority: 7,
+		onBasePower(basePower, pokemon, target, move) {
+			if (move.multihitType === 'parentalbond' && move.hit > 1) return this.chainModify(0.25);
+		},
+		onSourceModifySecondaries(secondaries, target, source, move) {
+			if (move.multihitType === 'parentalbond' && move.id === 'secretpower' && move.hit < 2) {
+				// hack to prevent accidentally suppressing King's Rock/Razor Fang
+				return secondaries.filter(effect => effect.volatileStatus === 'flinch');
+			}
+		},
+	},
+	inversionwarp: {
+		name: "Inversion Warp",
+		num: -1010,
+		
+	},
+	damagewarp: {
+		name: "Damage Warp",
+		num: -1011,
+		
+	},
+	epipelagicdeity: {
+		name: "Epipelagic Deity", 
+		num: -1012,
+		
+	},
+	bathypelagicdeity: {
+		name: "Bathypelagic Deity",
+		num: -1013,
+		
+	},
+	mesopelagicdeity: {
+		name: "Mesopelagic Deity",
+		num: -1014,
+		
+	},
+	venomdrain: {
+		name: "Venom Drain",
+		num: -1015,
+		
+	},
 };
